26e47228c5d7f4e012fddc62dda86f5c1eefd6e1
rootbabu <babuhacker360@gmail.com>
2024-11-17 09:03:32 +0530

    Add auth and user modules

--- document-management-backend/src/app.module.ts
+++ document-management-backend/src/app.module.ts
@@ -3,8 +3,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
 import { DocumentModule } from './modules/document/document.module';
 import { UploadModule } from './modules/upload/upload.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
@@ -17,11 +19,14 @@ import { UploadModule } from './modules/upload/upload.module';
       useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
         type: configService.get<string>('DB_TYPE') as any,
         url: configService.get<string>('DB_URL'),
+        database: configService.get<string>('DB_NAME'), 
         synchronize: false,
         useUnifiedTopology: true,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
       }),
     }),
+    AuthModule,
+    UserModule,
     DocumentModule,
     UploadModule,
   ],
