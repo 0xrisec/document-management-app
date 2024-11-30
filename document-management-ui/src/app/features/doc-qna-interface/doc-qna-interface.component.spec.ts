57ad75eb2dc111aa5979779752191f5353b72fb9
rootbabu <babuhacker360@gmail.com>
2024-12-01 00:35:41 +0530

    Update QnA interface component test file

--- document-management-ui/src/app/features/doc-qna-interface/doc-qna-interface.component.spec.ts
+++ document-management-ui/src/app/features/doc-qna-interface/doc-qna-interface.component.spec.ts
@@ -1,23 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 import { DocQnaInterfaceComponent } from './doc-qna-interface.component';
+import { DocumentViewerComponent } from '../document-management/document-viewer/document-viewer.component';
+import { QnaInterfaceComponent } from '../qna-interface/qna-interface.component';
+import { DocumentSidebarComponent } from '../document-management/document-sidebar/document-sidebar.component';
 
 describe('DocQnaInterfaceComponent', () => {
-  let component: DocQnaInterfaceComponent;
-  let fixture: ComponentFixture<DocQnaInterfaceComponent>;
+    let component: DocQnaInterfaceComponent;
+    let fixture: ComponentFixture<DocQnaInterfaceComponent>;
+    let activatedRouteStub: Partial<ActivatedRoute>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [DocQnaInterfaceComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [DocQnaInterfaceComponent, DocumentViewerComponent, QnaInterfaceComponent, DocumentSidebarComponent],
+            providers: [
+                { provide: ActivatedRoute, useValue: activatedRouteStub }
+            ]
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(DocQnaInterfaceComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(DocQnaInterfaceComponent);
+        component = fixture.componentInstance;
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create the component', () => {
+        expect(component).toBeTruthy();
+    });
 });
