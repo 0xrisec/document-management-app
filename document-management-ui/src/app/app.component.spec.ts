a824a180d6bb94c7d5a7cc50df42585895bdc478
rootbabu <babuhacker360@gmail.com>
2024-11-30 23:18:57 +0530

    Add unit test for login component

--- document-management-ui/src/app/app.component.spec.ts
+++ document-management-ui/src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('document-management-ui');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, document-management-ui');
-  });
 });
